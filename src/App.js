f6c3ed6c93a2b0156276d8fef75dc69833ed0fff
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-12-18 19:22:07 +0100

    Add axios, bootstrap, and react-router-dom dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import bannerNoticias from './Banner-noticias.jpg';
 
 function App() {
+  const [noticia, setNoticia] = useState({ titulo: '', desarrollo: '' });
+  const [noticiaId, setNoticiaId] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      axios.get(`http://127.0.0.1:8000/api/noticias/siguiente/${noticiaId}`)
+        .then(response => {
+          setNoticia(response.data);
+          setNoticiaId(response.data.id);
+        })
+        .catch(error => console.log(error));
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [noticiaId]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card" style={{ width: '18rem' }}>
+        <img src={bannerNoticias} className="card-img-top" alt="Avances de noticias" />
+        <div className="card-body">
+          <h5 className="card-title">{noticia.titulo}</h5>
+          <p className="card-text">{noticia.desarrollo}</p>
+        </div>
+      </div>
     </div>
   );
 }
