003f64b8e2aa8da06874dce388471aacb2aae594
Juan Carlos Sulbaran <sulbaranjc@gmail.com>
2023-12-30 18:45:54 +0100

    Add font-awesome stylesheet and update static news and footer styles

--- src/components/BloqueNoticiasEstaticas.js
+++ src/components/BloqueNoticiasEstaticas.js
@@ -1,16 +1,23 @@
 // BloqueNoticiasEstaticas.jsx
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import imagen01 from '../assets/noticia_Imagen_01.jpg';
+import imagen02 from '../assets/noticia_Imagen_02.jpg';
+import imagen03 from '../assets/noticia_Imagen_03.jpg';
+
 
 const BloqueNoticiasEstaticas = () => {
   // Ejemplo de noticias estáticas
   const noticiasEstaticas = [
-    { titulo: 'Noticia 1', descripcion: 'Descripción de la noticia 1...' },
-    { titulo: 'Noticia 2', descripcion: 'Descripción de la noticia 2...' },
-    { titulo: 'Noticia 3', descripcion: 'Descripción de la noticia 3...' },
+    { imagen : imagen01,
+      titulo: 'Descubren un virus desconocido hasta ahora en una lechuza muerta en Extremadura', 
+      descripcion: 'Investigadores encuentran un nuevo virus, al que han denominado BoBV-1, en un ave muerta: similar a otro patógeno que afecta a los canarios, no parece afectar al ser humano' },
+    { imagen : imagen02,
+      titulo: 'IA y abogacía: trabajo híbrido y conversacional permanente con algoritmos jurídicos', 
+      descripcion: 'Las tendencias para el sector es que se migre de un acto creativo, individual y relativamente lento, dominado por la odiosa mecanografía, a la escritura híbrida, conversacional y permanente, con avanzados algoritmos del lenguaje' },
+    { imagen : imagen03,
+      titulo: 'Cómo solicitar el voto por correo para las elecciones de Galicia 2024', descripcion: 'Para pedir el voto por correo deben tenerse en cuenta una serie de plazos. Este es el procedimiento que deberás seguir' },
     { titulo: 'Noticia 4', descripcion: 'Descripción de la noticia 4...' },
-    { titulo: 'Noticia 5', descripcion: 'Descripción de la noticia 5...' },
-    { titulo: 'Noticia 6', descripcion: 'Descripción de la noticia 6...' },
     // Agrega más noticias según sea necesario
   ];
 
@@ -19,6 +26,7 @@ const BloqueNoticiasEstaticas = () => {
       {noticiasEstaticas.map((noticia, index) => (
         <Card key={index} className="mb-3">
           <Card.Body>
+            <Card.Img variant="top" src={noticia.imagen} style={{ width: '100px', height: '100px' }} />
             <Card.Title>{noticia.titulo}</Card.Title>
             <Card.Text>{noticia.descripcion}</Card.Text>
           </Card.Body>
--- src/components/FooterApp.js
+++ src/components/FooterApp.js
@@ -1,19 +1,34 @@
 // FooterApp.jsx
 import React from 'react';
+import "./FooterApp.css"
 
 const FooterApp = () => {
   return (
-    <footer className="footer">
-      <div className="container">
-        <p>&copy; {new Date().getFullYear()} Tu Compañía. Todos los derechos reservados.</p>
-        <p>Contacto: info@tucompania.com</p>
-        <div className="social-links">
-          {/* Enlaces a redes sociales, puedes cambiarlos o agregar más */}
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
-        </div>
+  <footer className="footer ftcolor">
+  <div className="content">
+    <div className="top">
+      <div className="logo-details">
+        <i className="fab fa-slack"></i>
+        <span className="logo_name">JC Software</span>
       </div>
+      <div class="media-icons">
+        <a href="https://www.facebook.com/"><i className="fab fa-facebook-f"></i></a>
+        <a href="https://www.twitter.com/"><i className="fab fa-twitter"></i></a>
+        <a href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a>
+        <a href="https://www.linkedin.com/"><i className="fab fa-linkedin-in"></i></a>
+        <a href="https://www.youtube.com/"><i className="fab fa-youtube"></i></a>
+      </div>
+    </div>
+  </div>
+  <div className="bottom-details">
+    <div className="bottom_text">
+      <span className="copyright_text">Copyright © 2024 <a href="/copy">JC Software </a>All rights reserved</span>
+      <span className="policy_terms">
+        <a href="/privacy">Privacy policy</a>
+        <a href="/term">Terms & condition</a>
+      </span>
+    </div>
+  </div>
     </footer>
   );
 }
